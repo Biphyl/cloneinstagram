5ae81bbe9b46e469439cc69da512888692ad8173
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-06-09 13:05:08 +0300

    create add_comment and profile functions

--- social/views.py
+++ social/views.py
@@ -55,3 +55,34 @@ def comment(request, post_id):
             "comment_form":comment_form,
         }
         return render(request, 'posts.html', context)
+
+@login_required
+def add_comment(request, post_id):
+    posts = Post.objects.get(pk=post_id)
+    context={
+        "posts":posts,
+    }
+    return render(request, 'comments.html', context)
+
+
+@login_required
+def profile(request):
+    posts = Post.objects.all()
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been successfully updated')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+    'user_form':user_form,
+    'profile_form':profile_form,
+    'posts':posts,
+    }
+    return render(request, 'profile.html', context)
