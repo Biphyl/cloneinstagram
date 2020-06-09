c9e740d400f480fa6600a15bea163ae14050a6bc
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-06-09 12:49:32 +0300

    create Profile model

--- social/models.py
+++ social/models.py
@@ -41,3 +41,25 @@ class Post(models.Model):
         except ObjectDoesNotExist:
             raise Http404()
         return post
+
+class Profile(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_pics/')
+    bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    class Meta:
+        db_table = 'profile'
+
+    @receiver(post_save, sender=User)
+    def update_create_profile(sender,instance,created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+
+    def save_profile(self):
+        self.save()
