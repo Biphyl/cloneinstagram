9040720f9eac0977831bfff2ff3228138f7abc54
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-06-09 13:02:57 +0300

    create post function

--- social/views.py
+++ social/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse,get_object_or_404,HttpResponseRedirect
+from .models import Profile,Post,User,Comment,Following
+from django.contrib import messages
+from .forms import UserCreationForm,UserUpdateForm,CommentForm,ProfileUpdateForm,PostForm,RegisterForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def post(request):
+    posts = Post.objects.all()
+    users = User.objects.exclude(id=request.user.id)
+    comments = Comment.objects.all()
+    comment_form = CommentForm()
+
+    context = {
+        "posts":posts,
+        "comments":comments,
+        "users":users,
+        "comment_form":comment_form
+    }
+
+    return render(request, 'posts.html', context)
